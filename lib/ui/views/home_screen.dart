ef716efa0bbcb7bb4af0dc1abb28f7c2f7eb4f20
Dhruv245 <52127829+Dhruv245@users.noreply.github.com>
2019-12-27 13:24:44 +0530

    Initial commit

---
+++ lib/ui/views/home_screen.dart
@@ -0,0 +1,45 @@
+import '../resources/constants.dart';
+import 'package:disease_app/ui/views/news_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:nice_button/nice_button.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(Constants.appName),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Center(
+            child: Text("Hello World!"),
+          ),
+          NiceButton(
+            width: 255,
+            elevation: 8.0,
+            radius: 52.0,
+            text: "Lastest News",
+            background: Colors.blue,
+            onPressed: () => Navigator.push(
+                context,
+                PageRouteBuilder(
+                  pageBuilder: (_, __, ___) => NewsScreen(),
+                  transitionsBuilder: (_, animation, __, child) => FadeTransition(
+                    opacity: animation,
+                    child: child,
+                  ),
+                )
+            ),
+          ),
+        ]
+    )
+    );
+  }
+}
