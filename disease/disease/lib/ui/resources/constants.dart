d29c1d787da8c256cf887881ddcb8f98a8a7211f
Dhruv245 <52127829+Dhruv245@users.noreply.github.com>
2019-12-29 16:02:00 +0530

    fd

---
+++ disease/disease/lib/ui/resources/constants.dart
@@ -0,0 +1,7 @@
+class Constants {
+
+  static final String appName = "Disease App";
+  static final String newsScreenTitle = "News";
+  static final String newsApiKey = 'd80ac280dd944371b8ab8561874230b3'; 
+
+}
